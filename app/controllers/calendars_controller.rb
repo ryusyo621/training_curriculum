ba449941b7a055e7595eb835134b7df6c7019173
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-12 14:43:57 +0900

    wday_numの追加
    wday_numが未使用になっており、曜日が火曜日のみ表示されていました。

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,17 +29,17 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-  7.times do |x|
+    7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = @todays_date.wday
+      wday_num = @todays_date.wday + x
       if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = {month: (@todays_date + x).month, date:@todays_date.day + x, plans: today_plans, wday: wdays[@todays_date.wday]}
+      days = {month: (@todays_date + x).month, date:@todays_date.day + x, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
