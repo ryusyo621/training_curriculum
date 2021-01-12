edb2b07079962538750837fe25578934d9897c53
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-12 12:32:44 +0900

    シンボル型への修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -39,7 +39,7 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, :wday => wdays[@todays_date.wday]}
+      days = {month: (@todays_date + x).month, date:@todays_date.day + x, plans: today_plans, wday: wdays[@todays_date.wday]}
       @week_days.push(days)
     end
 
